b8fbc8acda73cb2ed305902cb5c3698dac75bf50
Gabriel Romero <gabrielromerods@hotmail.com>
2020-08-20 22:53:42 -0300

    FactoryMethod

---
+++ Creational/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar objetos de modo dinâmico em tempo de execução
+            FactoryMethod fm = new FactoryMethod();
+            Console.WriteLine("Liu Kang | SubZero | Scorpion");
+            Console.WriteLine();
+
+            string escolha = Console.ReadLine();
+            IPersonagem escolhido = fm.Escolher_Personagem(escolha);
+            Console.WriteLine("O escolhido foi: ");
+            escolhido.Escolhido();
+            Console.ReadLine();
+        }
+    }
+}
