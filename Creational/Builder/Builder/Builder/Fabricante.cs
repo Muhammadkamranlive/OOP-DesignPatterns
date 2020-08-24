2835b6d693bceeb4b05f27e65bf87521943adf28
Gabriel Romero <gabrielromerods@hotmail.com>
2020-08-24 17:55:55 -0300

    Builder

---
+++ Creational/Builder/Builder/Builder/Fabricante.cs
@@ -0,0 +1,16 @@
+﻿using Builder.Model.IBuild;
+using System;
+
+namespace Builder.Builder
+{
+    class Fabricante
+    {
+        public void Construtor(ICelular celular)
+        {
+            celular.BuildBateria();
+            celular.BuildCamera();
+            celular.BuildSistema();
+            celular.BuildTela();
+        }
+    }
+}
---
+++ Creational/Builder/Builder/Model/IBuild/ICelular.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Builder.Model.IBuild
+{
+    public interface ICelular
+    {
+        void BuildTela();
+        void BuildBateria();
+        void BuildSistema();
+        void BuildCamera();
+
+        Celular Celular { get; }
+    }
+}
