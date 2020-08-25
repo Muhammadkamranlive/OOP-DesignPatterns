e9ad2b152ecc8c5e8ae8b7fab6aaf309cba57cee
Gabriel Romero <gabrielromerods@hotmail.com>
2020-08-25 09:54:29 -0300

    Prototype

---
+++ Creational/Prototype/Prototype/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncionarioPermanente perm = new FuncionarioPermanente();
+            perm.Nome = "Joao";
+            perm.Idade = 30;
+            perm.Tipo = "Permanente";
+
+            FuncionarioPermanente perm2 = (FuncionarioPermanente)perm.Clone();
+            perm2.Idade = 40;
+            perm2.Nome = "Carlos";
+
+            Console.WriteLine("Detalhe do Funcionário");
+            Console.WriteLine($"Nome: {perm.Nome} e do clonado {perm2.Nome}");
+            Console.WriteLine($"Idade: {perm.Idade} e do clonado {perm2.Idade}");
+            Console.WriteLine($"Tipo: {perm.Tipo} e do clonado {perm2.Tipo}");
+            Console.ReadKey();
+        }
+    }
+}
